0a8e945baa3da2edd349289f63a9fe8bd2bb6073
luciano-freitas-melo <lucianodefreitasmelo@hotmail.com>
2026-04-13 23:50:51 -0300

    refactor: remove babel configuration and update jest settings
    - Deleted babel.config.js as it is no longer needed.
    - Updated jest.config.js to integrate with Next.js and improved alias handling.
    - Added new test command in package.json for running tests directly.
    - Updated dependencies in package.json for testing libraries and removed unused ones.
    - Deleted pnpm-lock.yaml as part of the transition to a new package management strategy.

--- jest.config.js
+++ jest.config.js
@@ -1,29 +1,38 @@
+const nextJest = require('next/jest');
+
+// Carrega o ambiente do Next.js (lê next.config.js e arquivos .env)
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+// Suas configurações de alias dinâmicos continuam normais
 const aliases = require('./settings/alias').reduce((acc, alias) => {
   acc[`^${alias.name}(.*)$`] = `<rootDir>${alias.path}$1`;
   return acc;
 }, {});
 
-module.exports = {
+/** @type {import('jest').Config} */
+const customJestConfig = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
   testRegex: '((\\.|/*.)(spec))\\.tsx?$',
   coverageDirectory: 'coverage',
-  preset: 'ts-jest',
-  transform: {
-    '^.+\\.(ts|tsx)$': 'babel-jest', // Adiciona suporte para arquivos TypeScript
-    '^.+\\.(js|jsx|mjs|cjs)$': 'babel-jest' // Adiciona suporte para arquivos JavaScript
-  },
-  transformIgnorePatterns: ['/node_modules/(?!(d3-array|d3-scale|@mui/x-charts|@babel/runtime)/)'],
+
+  testEnvironment: 'jest-environment-jsdom',
+
+  // Carrega os arquivos de setup
+  setupFiles: ['<rootDir>/tests/jestSetup.ts'],
+
+  // Mais tipos de expect para testes
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+
+  // Mapeia os aliases para os caminhos dos arquivos
   moduleNameMapper: {
     ...aliases,
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Mock de arquivos de estilo
-    '\\.svg$': '<rootDir>/__mocks__/svgMock.js', // Mock de arquivos SVG
-    '^next/image$': '<rootDir>/__mocks__/next/ImageMock.js' // Mock do next/image
+    '\\.svg$': '<rootDir>/__mocks__/svgMock.js',
+    '^next/image$': '<rootDir>/__mocks__/next/ImageMock.js'
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  setupFiles: ['<rootDir>/tests/jestSetup.ts'],
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-  testEnvironment: 'jsdom',
+
   reporters: [
     'default',
     [
@@ -35,3 +44,6 @@ module.exports = {
     ]
   ]
 };
+
+// Exporta a configuração combinando o padrão do Next com a configuração customizada
+module.exports = createJestConfig(customJestConfig);
