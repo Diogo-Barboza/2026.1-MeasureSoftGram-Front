b6548e960ea53fb24b039c9c678c967eaa091911
luciano-freitas-melo <lucianodefreitasmelo@hotmail.com>
2026-06-01 11:18:22 -0300

    test(jestSetup): adds i18 global mock
    -> updates snapshots that were using translations and not keys

--- tests/jestSetup.ts
+++ tests/jestSetup.ts
@@ -16,5 +16,16 @@ export const createMockedRoute = (
 // @ts-ignore
 global.fetch = jest.fn();
 
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    t: (key: string) => key,
+    // Adicionamos o objeto i18n mockado aqui:
+    i18n: {
+      language: 'pt-BR', // Simula que o idioma atual é português
+      changeLanguage: jest.fn(), // Finge a função de troca sem fazer nada
+    },
+  }),
+}));
+
 jest.mock('next/config', () => () => ({ publicRuntimeConfig: {} }));
 jest.spyOn(hooks, 'useRouter').mockImplementation(() => createMockedRoute({ layer: undefined, product: undefined }));
