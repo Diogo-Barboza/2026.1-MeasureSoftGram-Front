73bbc560deafd705c03f351df608fcfd7065451c
Giovanni Giampauli <giovanni.acg@gmail.com>
2026-06-07 19:12:47 -0300

    test(repository-header): add failing tests for #15 (RED)
    Prova que o modal do semaforo:
    - usa titulo e botao cancelar hardcoded em vez do i18n
    - nao exibe legenda explicando as faixas do semaforo

--- src/pages/products/[product]/repositories/[repository]/components/Header/tests/Header.spec.tsx
+++ src/pages/products/[product]/repositories/[repository]/components/Header/tests/Header.spec.tsx
@@ -62,6 +62,27 @@ describe('Header', () => {
 
   });
 
+  it('uses i18n keys for the modal title and cancel button', () => {
+    const { getByTestId, queryByText } = render(<Header />, { wrapper: AllTheProviders });
+
+    fireEvent.click(getByTestId('SettingsIcon'));
+
+    // Sob o mock de i18n, t(key) => key. Os textos do modal devem vir do
+    // namespace 'header', nao hardcoded em portugues.
+    expect(queryByText('edit_intervals')).toBeTruthy();
+    expect(queryByText('cancel')).toBeTruthy();
+  });
+
+  it('shows a legend describing the semaphore ranges', () => {
+    const { getByTestId, queryByText } = render(<Header />, { wrapper: AllTheProviders });
+
+    fireEvent.click(getByTestId('SettingsIcon'));
+
+    expect(queryByText('legend_bad')).toBeTruthy();
+    expect(queryByText('legend_regular')).toBeTruthy();
+    expect(queryByText('legend_good')).toBeTruthy();
+  });
+
   it('initialValues should be 0.33 and 0.66', () => {
     const { queryByText, getByTestId } = render(<Header />, { wrapper: AllTheProviders });
 
